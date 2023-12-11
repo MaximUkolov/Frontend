7e139766088cacaa93a00e3d4d361666d5297100
Max <0665232@mail.ru>
2023-12-11 21:05:26 +0300

    create JS

---
+++ JS/sem1/script.js
@@ -0,0 +1,14 @@
+myText = 'Текст из index.html';
+console.log(myText);
+
+
+const num = 123;     /*постоянная переменная*/
+console.log(num);
+
+let a = 5;
+a = 7;
+console.log(a);
+
+let b = 1 + 2 + 3;
+console.log(b);
+
