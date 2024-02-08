9e8d4f934bc28838513c2c78891237691beb2681
Max <0665232@mail.ru>
2024-02-08 19:11:40 +0300

    hw4 done

--- JS2/script.js
+++ JS2/script.js
@@ -298,32 +298,32 @@
 // Если элемент не выбран, добавить текст под чекбоксом “Необходимо согласиться с условиями”
 
 
-// console.log(agree); // js сам ловит id-шники
-// myForm.addEventListener('submit', function(event) {
-// event.preventDefault();
-// const checkbox = document.getElementById('agree');
-// const errorMessage = document.getElementById('error-message');
+console.log(agree); // js сам ловит id-шники
+myForm.addEventListener('submit', function(event) {
+event.preventDefault();
+const checkbox = document.getElementById('agree');
+const errorMessage = document.getElementById('error-message');
 
-// errorMessage.textContent = checkbox.checked ? '' : 'Необходимо согласиться с условиями';
-// // if (!checkbox.checked) {
-// // errorMessage.textContent = 'Необходимо согласиться с условиями';
-// // } else {
-// // errorMessage.textContent = '';
-// // // По идее, здесь должен быть код для отправки формы.
-// // }
-// });
-// document.getElementById('myForm').addEventListener('submit', function(event) {
-// event.preventDefault();
-// const checkbox = document.getElementById('agree');
-// const errorMessage = document.getElementById('error-message');
-
+errorMessage.textContent = checkbox.checked ? '' : 'Необходимо согласиться с условиями';
 // if (!checkbox.checked) {
 // errorMessage.textContent = 'Необходимо согласиться с условиями';
 // } else {
 // errorMessage.textContent = '';
 // // По идее, здесь должен быть код для отправки формы.
 // }
-// });
+});
+document.getElementById('myForm').addEventListener('submit', function(event) {
+event.preventDefault();
+const checkbox = document.getElementById('agree');
+const errorMessage = document.getElementById('error-message');
+
+if (!checkbox.checked) {
+errorMessage.textContent = 'Необходимо согласиться с условиями';
+} else {
+errorMessage.textContent = '';
+// По идее, здесь должен быть код для отправки формы.
+}
+});
 
 
 
