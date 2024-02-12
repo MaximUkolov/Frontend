c3e3e2f29854d6d26010009475e854003779a914
Max <0665232@mail.ru>
2024-02-12 23:54:57 +0300

    '

--- JS2/hw5/data.js
+++ JS2/hw5/data.js
@@ -1,6 +1,7 @@
 const dataInfo = `
 [
   {
+    "image": "https://cdn-icons-png.freepik.com/512/12894/12894963.png?ga=GA1.1.534298568.1699185848&",
     "code": "USD",
     "symbol": "&#36;",
     "rate": "48,171.153",
@@ -8,6 +9,7 @@ const dataInfo = `
     "rate_float": 48171.1532
   },
   {
+    "image": "https://cdn-icons-png.freepik.com/512/11343/11343960.png?ga=GA1.1.534298568.1699185848&",
     "code": "GBP",
     "symbol": "&pound;",
     "rate": "38,139.992",
@@ -15,6 +17,7 @@ const dataInfo = `
     "rate_float": 38139.9923
   },
   {
+    "image": "https://cdn-icons-png.freepik.com/512/2028/2028686.png?ga=GA1.1.534298568.1699185848&",
     "code": "EUR",
     "symbol": "&euro;",
     "rate": "44,662.27",
