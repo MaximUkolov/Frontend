38d76234a9ed3c1b2102fcdb596dfa87f80241f8
Max <0665232@mail.ru>
2024-02-16 22:01:00 +0300

    hw6 done

--- JS2/hw6/data.js
+++ JS2/hw6/data.js
@@ -7,7 +7,7 @@ const dataProducts = `
         "price_value": 300,
         "currency": "USD",
         "color": "red",
-        "size": "XL",
+        "size": "L",
         "quantity_value": 2,
         "image": "prod_1.jpg"
     },
@@ -25,4 +25,33 @@ const dataProducts = `
 ]
 `
 
-
+{/* <div class="product_box">
+<div class="box_left">
+  <img src="prod_2.jpg" alt="img1" class="img" />
+</div>
+<div class="box_right">
+  <a href="#" class="brand"
+    >Ellery X M'O <span class="name">capsule</span></a
+  >
+  <div class="box_info">
+    <div class="price">
+      <p>Price:</p>
+      <p class="price_text">
+        <span class="currency">$</span
+        ><span class="price_value_num">300</span>
+      </p>
+    </div>
+    <p class="color">Color: <span class="color_value">Red</span></p>
+    <p>
+      Size:
+      <select id="size" class="size">
+        <option value="#"></option>
+        <option value="M">M</option>
+        <option value="L">L</option>
+        <option value="XL">XL</option>
+      </select>
+    </p>
+    <div class="quantity">
+      <p>Quantity:</p>
+      <input type="number" class="quantity_value" min="0" />
+      <button type="submit" class="button">Ok</button> */}
