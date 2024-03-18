dda76e5bdcfd85f9724c640e94bee10f9e85a199
Max <0665232@mail.ru>
2024-03-18 19:00:17 +0300

    hw1 Node done

--- JS2/NodeJS/hw1.js
+++ JS2/NodeJS/hw1.js
@@ -4,19 +4,21 @@
 // — Также реализуйте обработку несуществующих роутов (404).
 // — * На каждой странице реализуйте счетчик просмотров. Значение счетчика должно увеличиваться на единицу каждый раз, когда загружается страница.
 
-const http = require("http");
-const { createElement } = require("react");
-const server = http.createServer(req, res);
-if (req.url === "/") {
-  res.writeHead(200, { "Content-Type": "text/html; charset=UTF-8" });
-  res.end("<h1>/</h1>");
-} else if (req.url === "/about") {
-  res.writeHead(200, { "Content-Type": "text/html; charset=UTF-8" });
-  res.end("<h1>/about</h1>");
-  document.querySelector("div");
-  const aHref = createElement('a')
-  aHref.textContent = 'На главную'
-  aHref.href = '127.0.0.1:4000'
-}
 
-server.listen(4000);
+const http = require("http");
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/html; charset=UTF-8" });
+    res.end("<h1>/</h1>");
+  } else if (req.url === "/about") {
+    res.writeHead(200, { "Content-Type": "text/html; charset=UTF-8" });
+    res.write("<h1>/about</h1>");
+    res.write('<a href="http://127.0.0.1:5000">На главную</a>');
+    res.end();
+  } else {
+    res.writeHead(404, { "Content-Type": "text/html; charset=UTF-8" });
+    res.end("<h1>Error (404)</h1>");
+  }
+});
+server.listen(5000);
+
