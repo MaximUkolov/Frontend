c9c0086889f4755f915f410fc6220016e2f406a7
Max <0665232@mail.ru>
2024-03-24 22:58:47 +0300

    hw3 done

--- JS2/NodeJS/hw3/index.js
+++ JS2/NodeJS/hw3/index.js
@@ -5,41 +5,77 @@
 // — Также значение счетчика должно загружаться из файла, когда запускается обработчик страницы
 // — Таким образом счетчик не должен обнуляться каждый раз, когда перезапускается сервер
 
-const express = require("express");
-const fs = require("fs");
+// const express = require("express");
+// const fs = require("fs");
 
+// const app = express();
+// let viewsData = {};
+
+// app.use((req, res, next) => {
+//   try {
+//     const data = fs.readFileSync("viewsData.json", "utf8");
+//     viewsData = JSON.parse(data);
+//   } catch (err) {
+//     console.error(err);
+//   }
+//   next();
+// });
+
+// app.use((req, res, next) => {
+//   fs.writeFileSync("viewsData.json", JSON.stringify(viewsData, null, 2));
+//   next();
+// });
+
+// app.get("/", (req, res) => {
+//   const url = "/";
+//   viewsData[url] = viewsData[url] ? viewsData[url] + 1 : 1;
+//   res.send(
+//     `<h1>Home Page - Views: ${viewsData[url]}</h1> <a href="/about">Link /about</a>`
+//   );
+// });
+
+// app.get("/about", (req, res) => {
+//   const url = "/about";
+//   viewsData[url] = viewsData[url] ? viewsData[url] + 1 : 1;
+//   res.send(
+//     `<h1>About Page - Views: ${viewsData[url]}</h1> <a href="/">Link /</a>`
+//   );
+// });
+
+
+const express = require('express');
+const fs = require('fs');
 const app = express();
-let viewsData = {};
 
-app.use((req, res, next) => {
-  try {
-    const data = fs.readFileSync("viewsData.json", "utf8");
-    viewsData = JSON.parse(data);
-  } catch (err) {
-    console.error(err);
-  }
-  next();
+
+app.get('/', (req, res) => {
+    let viewsData = loadViewsData();
+    const url = '/';
+    viewsData[url] = viewsData[url] ? viewsData[url] + 1 : 1;
+    saveViewsData(viewsData);
+    res.send(`<h1>Home Page - Views: ${viewsData[url]}</h1> <a href="/about">Link /about</a>`);
 });
 
-app.use((req, res, next) => {
-  fs.writeFileSync("viewsData.json", JSON.stringify(viewsData, null, 2));
-  next();
+app.get('/about', (req, res) => {
+    let viewsData = loadViewsData();
+    const url = '/about';
+    viewsData[url] = viewsData[url] ? viewsData[url] + 1 : 1;
+    saveViewsData(viewsData);
+    res.send(`<h1>About Page - Views: ${viewsData[url]}</h1> <a href="/">Link /</a>`);
 });
 
-app.get("/", (req, res) => {
-  const url = "/";
-  viewsData[url] = viewsData[url] ? viewsData[url] + 1 : 1;
-  res.send(
-    `<h1>Home Page - Views: ${viewsData[url]}</h1> <a href="/about">Link /about</a>`
-  );
-});
+function loadViewsData() {
+    try {
+        const data = fs.readFileSync('viewsData.json', 'utf8');
+        return JSON.parse(data);
+    } catch (err) {
+        console.error(err);
+        return {};
+    }
+}
 
-app.get("/about", (req, res) => {
-  const url = "/about";
-  viewsData[url] = viewsData[url] ? viewsData[url] + 1 : 1;
-  res.send(
-    `<h1>About Page - Views: ${viewsData[url]}</h1> <a href="/">Link /</a>`
-  );
-});
+function saveViewsData(viewsData) {
+    fs.writeFileSync('viewsData.json', JSON.stringify(viewsData, null, 2));
+}
 
 app.listen(4000);
